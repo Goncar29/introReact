720899744c45ba6173f5e368ff715cffabcae63b
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-02-10 11:13:38 -0300

    Se quitan archivos y se comienza a trabajar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import './App.css';
+
+const todos = [
+    { text: 'Cortar cebolla', completed: false },
+    { text: 'Tomar el curso de intro a React', completed: false },
+    { text: 'Llorar con la llorona', completed: false },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <React.Fragment>
+            {/* <TodoCounter /> */}
+            <h2>Has completado 2 de 3 TODOs</h2>
+            {/* <TOdoSearch /> */}
+            <input placeholder="Cebolla"></input>
+            {/* <TodoList>
+                {todos.map(todo => (
+                    <TodoItem />
+                ))}
+            </TodoList> */}
+
+            {/* <CreateTodoButton /> */}
+            <button>+</button>
+        </React.Fragment>
+    );
 }
 
 export default App;
