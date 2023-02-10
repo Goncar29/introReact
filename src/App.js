fec31036403ab2ecae0a14c411a20ab4942038f1
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-02-10 16:13:39 -0300

    Se establecieron estilos para cada componente

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { CreateTodoButton } from './CreateTodoButton';
 // import './App.css';
 
 const todos = [
-    { text: 'Cortar cebolla', completed: false },
+    { text: 'Cortar cebolla', completed: true },
     { text: 'Tomar el curso de intro a React', completed: false },
     { text: 'Llorar con la llorona', completed: false },
 ];
@@ -21,7 +21,11 @@ function App() {
             {/* <input placeholder="Cebolla"></input> */}
             <TodoList>
                 {todos.map(todo => (
-                    <TodoItem key={todo.text} text={todo.text} />
+                    <TodoItem 
+                    key={todo.text} 
+                    text={todo.text} 
+                    completed={todo.completed}
+                    />
                 ))}
             </TodoList>
 
