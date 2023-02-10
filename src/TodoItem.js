659f278344bd652b571af16b33fb76bb5b013272
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-02-10 16:13:59 -0300

    Se establecieron estilos para cada componente

---
+++ src/TodoItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './TodoItem.css';
+
+function TodoItem(props){
+    return (
+        <li className="TodoItem">
+            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
+                √
+            </span>
+            <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
+                {props.text}
+            </p>
+            <span className="Icon Icon-delete">
+                x
+            </span>
+        </li>
+    );
+}
+
+export { TodoItem };
