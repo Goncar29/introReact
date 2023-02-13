f2886cf13f32118ea6ceb07d715b2838b2eff6d5
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-02-13 16:18:02 -0300

    Manejo de eventos

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,8 +2,16 @@ import React from 'react';
 import './CreateTodoButton.css'
 
 function CreateTodoButton(props){
+    const onClickButton = () => {
+        alert('Aqui se deberia abrir el modal!')
+    }
     return(
-        <button className='CreateTodoButton'>+</button>
+        <button 
+            className='CreateTodoButton'
+            onClick={onClickButton}
+        >
+            +
+        </button>
     );
 }
 
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,8 +2,15 @@ import React from "react";
 import './TodoSearch.css'
 
 function TodoSearch(){
+    const onSearchValueChanged = (event) => {
+        console.log(event.target.value)
+    }
     return (
-        <input className="TodoSearch" placeholder="Cebolla" />
+        <input 
+            className="TodoSearch" 
+            placeholder="Cebolla" 
+            onChange={onSearchValueChanged}
+        />
     );
 };
 
