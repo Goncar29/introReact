38c5dc6c87d3f8a257dc30cd5560463efc058640
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-02-13 17:01:32 -0300

    Se agregan archivos de TodoSection

---
+++ src/TodoSection.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './TodoSection.css'
+
+function TodoSection(props) {
+    return(
+        <section className='TodoSection'>
+            <img src="https://i.pinimg.com/564x/7d/5d/e6/7d5de604779b913734a056db644899f6.jpg" />
+        </section>
+    );
+}
+
+export { TodoSection };
