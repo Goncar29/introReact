12578ca490ac78decd7d685b079ffab85050bf3a
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-02-13 17:00:19 -0300

    Manejo del estado

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,16 +2,21 @@ import React from "react";
 import './TodoSearch.css'
 
 function TodoSearch(){
+    const [searchValue, setSearchValue] = React.useState('Juan');
+
     const onSearchValueChanged = (event) => {
         console.log(event.target.value)
+        setSearchValue(event.target.value)
     }
-    return (
+    return [
         <input 
             className="TodoSearch" 
             placeholder="Cebolla" 
+            value={searchValue}
             onChange={onSearchValueChanged}
-        />
-    );
+        />,
+        <p>{searchValue}</p>
+    ];
 };
 
 export { TodoSearch };
