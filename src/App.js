6eff113d86da25145833172b3fa42dd669f3d33b
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-02-14 19:12:32 -0300

    Realizamos la funcionalidad de completar la tarea y eliminarlos

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ const defaultTodos = [
 function App() {
     // Estado inicial de nuestros TODOs
     const [todos, setTodos] = React.useState(defaultTodos);
+    // El estado de nuestra búsqueda
     const [searchValue, setSearchValue] = React.useState('');
 
     // Cantidad de TODOs completados
@@ -24,17 +25,41 @@ function App() {
     // Cantidad total de TODOs
     const totalTodos = todos.length;
 
+    // Creamos una nueva variable en donde guardaremos las coincidencias con la búsqueda
     let searchedTodos = [];
 
+    // Lógica para filtrar, si el valor del input es 0 renderiza todos sino renderiza lo demas 
     if(!searchValue.length >= 1){
         searchedTodos = todos;
     } else{
+        //Si escribimos en el buscador lo
         searchedTodos = todos.filter(todo => {
             const todoText = todo.text.toLowerCase()
             const searchText = searchValue.toLowerCase()
             return todoText.includes(searchText)
         })
     }
+
+    //Cada vez que reciba un texto va a buscar en toda la lista de todo cual todo cumple con la condicion
+    const completeTodo = (text) => {
+        const todoIndex = todos.findIndex(todo => todo.text === text)
+        // creamos una nueva lista de todos (las mismas)
+        const newTodos = [...todos];
+        //y marcamos a ese todo que cumple con la condicion el valor true
+        newTodos[todoIndex].completed = true;
+        // despues mandamos a renderizar el nuevo estado
+        setTodos(newTodos);
+    };
+
+    const deleteTodo = (text) => {
+        const todoIndex = todos.findIndex(todo => todo.text === text)
+        // creamos una nueva lista de todos (las mismas)
+        const newTodos = [...todos];
+        //con el metodo splice quitamos el todo
+        newTodos.splice(todoIndex, 1)
+        // despues mandamos a renderizar el nuevo estado
+        setTodos(newTodos);
+    };
 
     return (
         <React.Fragment>
@@ -55,6 +80,8 @@ function App() {
                     key={todo.text} 
                     text={todo.text} 
                     completed={todo.completed}
+                    onComplete={() => completeTodo(todo.text)}
+                    onDelete={() => deleteTodo(todo.text)}
                     />
                 ))}
             </TodoList>
