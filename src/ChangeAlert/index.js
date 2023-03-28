e044e4c4270ecfa3a9c3392ca8a67a2658674052
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2023-03-28 15:58:32 -0300

    Creamos estilos a ChangeAlert y terminamos con la sincronizacion

--- src/ChangeAlert/index.js
+++ src/ChangeAlert/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { withStorageListener } from "./withStorageListener";
+import './ChangeAlert.css'
 
 function ChangeAlert({ show, toggleShow }){
     if(show){
         return ( 
-            <div>
-                <p>Hubo cambios!</p>
-                <button
-                onClick={() => toggleShow(false)}>
-                    Volver a cargar
-                </button>
+            <div className="ChangeAlert-bg">
+                <div className="ChangeAlert-container">
+                    <p>Parece que cambiaste tus TODOs en otra pestaña o ventana del navegador</p>
+                    <p>¿Quieres sincronizar tus TODOs?</p>
+                    <button
+                        className="TodoForm-button TodoForm-button--add"
+                        onClick={toggleShow}
+                    >
+                        Sincronizar!
+                    </button>
+                </div>
             </div>
         );
     } else{
--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -14,7 +14,7 @@ function TodoList(props){
             
             {(!!props.totalTodos && !props.searchedTodos.length) && props.onEmptySearchResults(props.searchText)}
 
-            {props.searchedTodos.map(renderFunc)}
+            {(!props.loading && !props.error) && props.searchedTodos.map(renderFunc)}
             
             <ul>
                 {props.children}
